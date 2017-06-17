8c8e71a4ba9a9421b5d7be8c8d9711ca85d299f2
JafarAkhondali <jafar.akhondali@yahoo.com>
2017-06-17 15:16:27 +0430

    Block scoped extra script executions and global namespace for firefox incompatibility with chrome.

--- chrome-extension/scripts/getall.js
+++ chrome-extension/scripts/getall.js
@@ -1,25 +1,28 @@
-let BrowserNameSpace;
-if(typeof browser !== 'undefined' )
-    BrowserNameSpace = browser ;
-else if(typeof chrome !== 'undefined' )
-    BrowserNameSpace = chrome;
+{
+    if(typeof BrowserNameSpace === "undefined"){
+        BrowserNameSpace;
+        if(typeof browser !== 'undefined' )
+            BrowserNameSpace = browser ;
+        else if(typeof chrome !== 'undefined' )
+            BrowserNameSpace = chrome;
+    }
 
+    function sendToExtension(msg) {
+        BrowserNameSpace.runtime.sendMessage({
+            type:"getSelected",
+            message: msg
+        });
+    }
 
-function sendToExtension(msg) {
-    BrowserNameSpace.runtime.sendMessage({
-        type:"getSelected",
-        message: msg
-    });
-}
-
-let links = [];
+    let links = [];
 
 //Get links of anchor tags
-let anchor_elements = document.querySelectorAll("a");
-for(let i=0; i<anchor_elements.length; i++) {
-    links.push(anchor_elements[i].href);
-}
+    let anchor_elements = document.querySelectorAll("a");
+    for(let i=0; i<anchor_elements.length; i++) {
+        links.push(anchor_elements[i].href);
+    }
 
 
 //Send html selection back to extension
-sendToExtension(links);
+    sendToExtension(links);
+}
--- chrome-extension/scripts/getselected.js
+++ chrome-extension/scripts/getselected.js
@@ -1,22 +1,26 @@
-let BrowserNameSpace;
-if(typeof browser !== 'undefined' )
-    BrowserNameSpace = browser ;
-else if(typeof chrome !== 'undefined' )
-    BrowserNameSpace = chrome;
+{
+    if(typeof BrowserNameSpace === "undefined"){
+        BrowserNameSpace;
+        if(typeof browser !== 'undefined' )
+            BrowserNameSpace = browser ;
+        else if(typeof chrome !== 'undefined' )
+            BrowserNameSpace = chrome;
+    }
 
-function sendToExtension(msg) {
-    BrowserNameSpace.runtime.sendMessage({
-        type:"getSelected",
-        message: msg
-    });
+    function sendToExtension(msg) {
+        BrowserNameSpace.runtime.sendMessage({
+            type:"getSelected",
+            message: msg
+        });
+    }
+
+    let selectedNode = window.getSelection().getRangeAt(0).cloneContents();
+    let nodes = selectedNode.querySelectorAll("a");
+    let links = [];
+    for(let i=0;i<nodes.length;i++){
+        links.push(nodes[i].href);
+    }
+
+    //Send html selection back to extension
+    sendToExtension(links);
 }
-
-let selectedNode = window.getSelection().getRangeAt(0).cloneContents();
-let nodes = selectedNode.querySelectorAll("a");
-let links = [];
-for(let i=0;i<nodes.length;i++){
-    links.push(nodes[i].href);
-}
-
-//Send html selection back to extension
-sendToExtension(links);
