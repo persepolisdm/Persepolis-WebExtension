b65c079ed1997d95ce1abc71bdf8aeb41b409a26
JafarAkhondali <jafar.akhondali@yahoo.com>
2019-01-19 19:05:45 +0330

    + Create packager.sh to automatically create packages required by MDN and Chrome store + Improve listener on keywords + Fix interruption config bug

---
+++ packager.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+EXPATH="WebExtension"
+
+#Create firefox package, which expects all files directly
+cp firefox_manifest.json $EXPATH/manifest.json
+
+
+rm -f ./firefox.zip
+cd ./$EXPATH/
+zip ../firefox.zip * -r
+cd ..
+
+
+cp chrome_manifest.json WebExtension/manifest.json
+rm -f ./chrome.zip
+zip chrome.zip $EXPATH/ -r
